f466619a99df9dc26f1c032493991e90073fb6aa
ilan.shavit@gmail.com <Ilan Shavit>
2023-03-07 16:36:28 +0200

    First Commit

---
+++ short_timer.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#sudo apt install espeak
+/home/ilan/Bin/Pilatis/say "Listen listen... 32 minutes... End Practice"
+espeak -s 115 "Listen listen... Start Practice... "
+sleep 293
+espeak -s 115 "Listen listen... 5 minutes...Listen listen... 5 minutes"
+sleep 293
+espeak -s 115 "Listen listen... 10 minutes... Listen listen... 10 minutes"
+sleep 293
+espeak -s 115 "Listen listen... 15 minutes... Listen listen... 15 minutes"
+sleep 293
+espeak -s 115 "Listen listen... 20 minutes... Listen listen... 20 minutes"
+sleep 293
+espeak -s 115 "Listen listen... 25 minutes... Listen listen... 25 minutes"
+sleep 293
+espeak -s 115 "Listen listen... 30 minutes... Listen listen... 30 minutes"
+sleep 120
+espeak -s 115 "Listen listen... 32 minutes... End Practice"
---
+++ start_timer.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#sudo apt install espeak
+espeak -s 115 "Test... Test.. Test..."
+sleep 5
+espeak -s 115 "Listen listen... Start Warning"
+sleep 733
+espeak -s 115 "Listen listen... Start Practice... "
+sleep 293
+espeak -s 115 "Listen listen... 5 minutes...Listen listen... 5 minutes"
+sleep 293
+espeak -s 115 "Listen listen... 10 minutes... Listen listen... 10 minutes"
+sleep 293
+espeak -s 115 "Listen listen... 15 minutes... Listen listen... 15 minutes"
+sleep 293
+espeak -s 115 "Listen listen... 20 minutes... Listen listen... 20 minutes"
+sleep 293
+espeak -s 115 "Listen listen... 25 minutes... Listen listen... 25 minutes"
+sleep 293
+espeak -s 115 "Listen listen... 30 minutes... Listen listen... 30 minutes"
+sleep 120
+espeak -s 115 "Listen listen... 32 minutes... End Practice"
